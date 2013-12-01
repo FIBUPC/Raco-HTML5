d32331a5fa55fe0aa78d400dc0b6ef4aede7c7ab
Cristian Ortega <Cristian@Mac-mini-de-Cristian.local>
2013-12-01 14:41:06 +0100

    Removing www

--- iOS/www/js/libraries/platform/windowsphone/fileBridge.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	cordova.addConstructor(function(){
-		window.plugins.fileBridge = {
-			getFileContents: function(path, okCallback, errorCallback) {
-				cordova.exec(okCallback, errorCallback, "FileBridge", "getFileContents", { path: path });
-			}
-		};
-	});
-})();
